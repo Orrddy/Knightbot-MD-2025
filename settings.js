5e7ab0ec5073f29cd8b91d107d3b6149e9aceceb
Odinaka Chukwu <80051778+Orrddy@users.noreply.github.com>
2025-02-18 10:49:12 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'Knight',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Orrddy",
+  botOwner: 'Orrddy Cloud', // Your name
+  ownerNumber: '2347056284216', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
